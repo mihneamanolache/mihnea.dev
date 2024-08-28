aea9536940a3153c5ce779afbf63af8f43ee0155
Mihnea-Octavian Manolache <mihnea.manolache@yahoo.com>
2024-08-28 09:08:02 -0400

    bugfix: add page exports

---
+++ src/app/mobile-proxies/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MobileProxies() {
+    return (
+        <main className="flex flex-col items-center justify-center min-h-[90vh] mt-8 px-5 md:px-32 text-center">
+        </main>
+    )
+}
+
---
+++ src/app/web-scraper-api/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function WebScraperApi() {
+    return (
+        <main className="flex flex-col items-center justify-center min-h-[90vh] mt-8 px-5 md:px-32 text-center">
+        </main>
+    )
+}
+
+
