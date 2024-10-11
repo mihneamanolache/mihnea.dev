6f2b378f071b55bfb62cd34cd9b01624aeec1c7a
Mihnea-Octavian Manolache <mihnea.manolache@yahoo.com>
2024-10-11 13:21:49 +0300

    add netcup affiliate page to sitemap

--- src/constants/sitemap-pages.tsx
+++ src/constants/sitemap-pages.tsx
@@ -1,25 +1,31 @@
 export default [
   {
     "url": "https://www.mihnea.dev/mobile-proxies",
-    "lastModified": "2024-08-29T10:58:17.467Z",
+    "lastModified": "2024-10-11T10:21:19.163Z",
+    "changeFrequency": "monthly",
+    "priority": 0.8
+  },
+  {
+    "url": "https://www.mihnea.dev/netcup-vouchers",
+    "lastModified": "2024-10-11T10:21:19.164Z",
     "changeFrequency": "monthly",
     "priority": 0.8
   },
   {
     "url": "https://www.mihnea.dev/ip-to-domain",
-    "lastModified": "2024-08-29T10:58:17.467Z",
+    "lastModified": "2024-10-11T10:21:19.164Z",
     "changeFrequency": "monthly",
     "priority": 0.8
   },
   {
     "url": "https://www.mihnea.dev/about",
-    "lastModified": "2024-08-29T10:58:17.467Z",
+    "lastModified": "2024-10-11T10:21:19.164Z",
     "changeFrequency": "monthly",
     "priority": 0.8
   },
   {
     "url": "https://www.mihnea.dev/web-scraper-api",
-    "lastModified": "2024-08-29T10:58:17.467Z",
+    "lastModified": "2024-10-11T10:21:19.164Z",
     "changeFrequency": "monthly",
     "priority": 0.8
   }
